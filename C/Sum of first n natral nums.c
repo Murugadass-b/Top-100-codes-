19f74e4afafdde6f2e4e4716e57766ec04018fd8
murugadass-b <murugadassbala@gmail.com>
2023-10-25 22:55:42 +0530

    made changes in sum of first n natural nums

--- C/Sum of first n natral nums.c
+++ C/Sum of first n natral nums.c
@@ -3,11 +3,11 @@ void main(){
     int n,sum=0;
     scanf("%d",&n);
     //method 1
-    // for(int i=1;i<=n;i++){
+    // for(int i=1;i<=n;i++){//time complexity:O(n)
     //     sum+=i;
     // }
     //method 2 using formula
-    sum = (n*(n+1))/2;
+    sum = (n*(n+1))/2;//time complexity:O(1)
 
 
     printf("%d",sum);
